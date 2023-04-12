2a13887a4166ac374d393514b17684dff2b6c54a
Ilya <solohin.i@gmail.com>
2023-04-12 15:53:39 +0000

    favicon and gtag

--- static/design/menu.js
+++ static/design/menu.js
@@ -9,3 +9,15 @@ close.addEventListener('click', () => {
 burger.addEventListener('click', () => {
     menu.style.transform = `translateX(${0}%)`
 });
+
+document.querySelectorAll('a[href*="://t.me/p2pcloud" i]').forEach(anchor => {
+    anchor.setAttribute('target', '_blank');
+    anchor.setAttribute('onclick', "gtag('event','telegram_click')");
+})
+
+document.querySelectorAll('a[href="/panel.html"]').forEach(anchor => {
+    anchor.setAttribute('onclick', "gtag('event','app_click')");
+})
+document.querySelectorAll('a[href*="app.p2pcloud.io"]').forEach(anchor => {
+    anchor.setAttribute('onclick', "gtag('event','app_click')");
+})
